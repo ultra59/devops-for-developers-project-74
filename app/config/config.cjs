9ecbae16aae126028a11eacbb5edff5ed0464e0b
ultra59 <elenataft2016@gmail.com>
2026-03-31 03:25:25 +0300

    самый последний дэп

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,28 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-  },
-  production: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
-  },
-  test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
-  },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
+	development: {
+		client: "pg",
+		connection: {
+			host: process.env.DATABASE_HOST,
+			database: process.env.DATABASE_NAME,
+			user: process.env.DATABASE_USERNAME,
+			password: process.env.DATABASE_PASSWORD,
+		},
+	},
+
+	production: {
+		client: "pg",
+		connection: {
+			host: process.env.DATABASE_HOST,
+			database: process.env.DATABASE_NAME,
+			user: process.env.DATABASE_USERNAME,
+			password: process.env.DATABASE_PASSWORD,
+			port: process.env.DATABASE_PORT,
+		},
+	},
+
+	test: {
+		client: "pg",
+		connection: {
+			host: process.env.DATABASE_HOST,
+			database: process.env.DATABASE_NAME,
+			user: process.env.DATABASE_USERNAME,
+			password: process.env.DATABASE_PASSWORD,
+		},
+	},
 };
